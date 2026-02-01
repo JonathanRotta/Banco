306971cada6b88ccd72eb6217dbb739ff6909859
Jonathan Rotta <jrotta19@gmail.com>
2026-02-01 14:17:52 -0300

    Estrutura inicial

---
+++ Banco/ContaBancaria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banco
+{
+    public class ContaBancaria
+    {
+        public int NumeroConta {  get; set; }
+        private int Saldo { get; set; }
+
+        public Pessoa Titular { get; set; }
+
+        public ContaBancaria(int numeroConta, Pessoa titular)
+        {
+            NumeroConta = numeroConta;
+            Titular = titular;
+
+        }
+
+        private void Deposito(int saldo) { 
+            Saldo = saldo;
+        }
+        public void Saque(int saldo)
+        {
+            Saldo -= saldo;
+            Saldo -= saldo;
+        }
+    }
+}
---
+++ Banco/Pessoa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Banco
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public int Carteira { get; set; }
+
+
+        public Pessoa(string Nome, int Idade)
+        {
+            this.Nome = Nome;
+            this.Idade = Idade;
+        }
+
+
+    }
+}
